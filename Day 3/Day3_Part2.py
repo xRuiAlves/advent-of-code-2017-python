9c2f5fdc8e1f442d260fef118ab4e558ba5d593e
xRuiAlves <up201606746@fe.up.pt>
2017-12-03 09:19:30 +0000

    Improved Day 3 :: Part 2 - Now fully automatic

--- Day 3/Day3_Part2.py
+++ Day 3/Day3_Part2.py
@@ -1,18 +1,52 @@
-import re
-
-try:
-    file = open("A141481" , "r")
-except IOError:
-    print("*** ERROR: Could not open file for reading #A141481 number sequence values ***")
-    raise SystemExit
-
 input_val = 347991
-input_line = None
-term = None
+
+# Create the matrix
+matrix = [[0 for i in range (0,200)] for j in range(0,200)]
+
+# Set the starting  point (Level 1)
+level = 1
+x = 100
+y = 100
+matrix[x][y] = 1
+print(matrix[x][y])
 
 while True:
-    input_line = file.readline()
-    term = re.findall("\d+" , input_line)
-    if (int(term[1]) > input_val):
-        print(term[1])
+    # Go to the next level (by summing one to X)
+    level += 1
+    x += 1
+
+    # Compute that point
+    matrix[x][y] = matrix[x-1][y] + matrix[x-1][y-1] + matrix[x][y-1] + matrix[x+1][y-1] + matrix[x+1][y] + matrix[x+1][y+1] + matrix[x][y+1] + matrix[x-1][y+1]
+    print(matrix[x][y])
+
+    if(matrix[x][y] > input_val):
         break
+
+    # Compute the remaining points in this level
+    for i in range(0,(level-1)*2 - 1):
+        y -= 1
+        matrix[x][y] = matrix[x-1][y] + matrix[x-1][y-1] + matrix[x][y-1] + matrix[x+1][y-1] + matrix[x+1][y] + matrix[x+1][y+1] + matrix[x][y+1] + matrix[x-1][y+1]
+        print(matrix[x][y])
+        if(matrix[x][y] > input_val):
+            exit()
+
+    for i in range(0,(level-1)*2):
+        x -= 1
+        matrix[x][y] = matrix[x-1][y] + matrix[x-1][y-1] + matrix[x][y-1] + matrix[x+1][y-1] + matrix[x+1][y] + matrix[x+1][y+1] + matrix[x][y+1] + matrix[x-1][y+1]
+        print(matrix[x][y])
+        if(matrix[x][y] > input_val):
+            exit()
+
+    for i in range(0,(level-1)*2):
+        y += 1
+        matrix[x][y] = matrix[x-1][y] + matrix[x-1][y-1] + matrix[x][y-1] + matrix[x+1][y-1] + matrix[x+1][y] + matrix[x+1][y+1] + matrix[x][y+1] + matrix[x-1][y+1]
+        print(matrix[x][y])
+        if(matrix[x][y] > input_val):
+            exit()
+
+    for i in range(0,(level-1)*2):
+        x += 1
+        matrix[x][y] = matrix[x-1][y] + matrix[x-1][y-1] + matrix[x][y-1] + matrix[x+1][y-1] + matrix[x+1][y] + matrix[x+1][y+1] + matrix[x][y+1] + matrix[x-1][y+1]
+        print(matrix[x][y])
+        if(matrix[x][y] > input_val):
+            exit()
